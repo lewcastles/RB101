e3915cbb449cdb81526a691c969569629a00cafa
Lewis Castles <castles.lewis@gmail.com>
2019-03-11 11:12:49 -0700

    refactor from srdans review

--- Small Problems/mortgage_loan_calc/mortgage_loan_calc.rb
+++ Small Problems/mortgage_loan_calc/mortgage_loan_calc.rb
@@ -1,5 +1,11 @@
+# strengthen your user-input validation spaces for my name
+
 def prompt(msg)
   puts "=> #{msg}"
+end
+
+def valid_name_format?(str)
+  str.match(/^[A-Z]+[ ]?[A-Z]*$/i) && !str.empty?
 end
 
 def valid_currency_format?(num)
@@ -16,7 +22,7 @@ def valid_percentage_format?(num)
   num.match(/^[+]?[%]?[0-9]*[.]?[0-9]*[%]?$/) && !num.empty?
 end
 
-def zero_neg_notice(num)
+def display_zero_neg_notice(num)
   if num.match(/^[$]?[0]*[.]?[0]{0,2}$/) || num.to_i < 0
     prompt('Input must be greater than zero.')
   else
@@ -24,12 +30,23 @@ def zero_neg_notice(num)
   end
 end
 
-def neg_notice(num)
+def display_neg_notice(num)
   if num.to_i < 0
-    prompt('Input must zero or positive.')
+    prompt('Input must be zero or positive.')
   else
     prompt('Input not valid. Please try again.')
   end
+end
+
+def display_welcome_message
+  (system 'clear') || (system 'cls')
+  puts '======Welcome to the Loan Calculator======'
+end
+
+def display_payment_message(name, mnthly_payments, total, duration)
+  prompt("Thank you #{name}, your monthly payments will be #{mnthly_payments}")
+  prompt("per month for #{duration} months with a total repayment of #{total}")
+  puts
 end
 
 def retrieve_loan_amt
@@ -39,7 +56,7 @@ def retrieve_loan_amt
     puts
     return loan_val.delete('$').to_f if valid_currency_format?(loan_val)
 
-    zero_neg_notice(loan_val)
+    display_zero_neg_notice(loan_val)
   end
 end
 
@@ -50,7 +67,7 @@ def retrieve_mnthly_int_rate
     puts
     return apr_val.delete('%').to_f / 1200 if valid_percentage_format?(apr_val)
 
-    neg_notice(apr_val)
+    display_neg_notice(apr_val)
   end
 end
 
@@ -61,53 +78,50 @@ def retrieve_loan_duration
     puts
     return duration.to_i if valid_posinteger_format?(duration)
 
-    zero_neg_notice(duration)
+    display_zero_neg_notice(duration)
   end
 end
 
 def retrieve_username
   loop do
-    puts '======Welcome to the Loan Calculator======'
     prompt('Please enter your name:')
     name = gets.chomp
     puts
-    return name unless name.empty?
+    return name.split.map(&:capitalize).join(' ') if valid_name_format?(name)
 
     prompt('We didnt catch your name, please try again')
   end
 end
 
 def prompt_user_repeat
-  prompt("Would you like to calculate another loan? (input 'Y' to repeat)")
-  gets.chomp.match(/^[y]?$/i)
+  prompt('Would you like to calculate another loan?')
+  prompt("(Input 'Y' to repeat, all other keys will close the program)")
+  repeat = gets.chomp
+  repeat.match(/^[y]?$/i) && !repeat.empty?
 end
 
 # MAIN PROCESS START --------------------------------------------------------
 
-(system 'clear') || (system 'cls') # clear screen in multiple os?? for clarity
-
-# username, probably minimal validation here, not critical to function.
-name = retrieve_username.capitalize
+display_welcome_message
+name = retrieve_username
 
 loop do
   prompt("#{name}, let us take a few moments to gather some information")
   # retrieve sanitized inputs from user in formula correct units
-  p = retrieve_loan_amt
-  j = retrieve_mnthly_int_rate
-  n = retrieve_loan_duration
+  loan_amt = retrieve_loan_amt
+  int_rate = retrieve_mnthly_int_rate
+  duration = retrieve_loan_duration
 
-  m = if j == 0.0
-        p / n # calculate interest-free equal payments
-      else
-        p * (j / (1 - (1 + j)**-n)) # calculation from formula
-      end
-  mnthly_payments = format('$%.2f', m.round(2))
-  total_to_pay = format('$%.2f', m.round(2) * n)
+  payments = if int_rate == 0.0
+               loan_amt / duration # calculate interest-free equal payments
+             else
+               loan_amt * (int_rate / (1 - (1 + int_rate)**-duration))
+             end
+  mnthly_payments = format('$%.2f', payments.round(2))
+  total = format('$%.2f', payments.round(2) * duration)
   # 2dp format possibly some rounding errors here, future improvement maybe.
 
-  prompt("Thank you #{name}, your monthly payments will be #{mnthly_payments}")
-  prompt("per month for #{n} months with a total repayment of #{total_to_pay}")
-  puts
+  display_payment_message(name, mnthly_payments, total, duration)
 
   break unless prompt_user_repeat # user does not wish to repeat then break loop
 
